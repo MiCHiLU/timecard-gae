39968f32e58e5a7a27af75edbe16d2289f51a166
ENDOH takanao <djmchl@gmail.com>
2014-02-09 17:03:48 +0700

    Google Cloud Endpointsの実装クラス名の文字列定義

--- gae/main_api/v1/__init__.py
+++ gae/main_api/v1/__init__.py
@@ -4,3 +4,7 @@ from project import *
 from issue import *
 from workload import *
 from comment import *
+
+api_classes = api.get_api_classes()
+api_class_names = [cls.definition_name() for cls in api_classes]
+api_class_names_string = " ".join(api_class_names)
