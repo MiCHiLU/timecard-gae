926fddb4f927751eda73a363b026bdb50014430f
ENDOH takanao <djmchl@gmail.com>
2014-02-15 17:49:34 +0700

    refactor

--- gae/dart/lib/timecard.dart
+++ gae/dart/lib/timecard.dart
@@ -20,7 +20,6 @@ class Controller {
   final REVOKE_URL = "https://accounts.google.com/o/oauth2/revoke?token=";
   GoogleOAuth2 auth;
   Timecard endpoint;
-  var token;
   var user;
 
   Controller() {
@@ -31,7 +30,7 @@ class Controller {
   }
 
   bool logged_in() {
-    if (token == null) {
+    if (auth.token == null) {
       return false;
     } else {
       return true;
@@ -43,19 +42,17 @@ class Controller {
   }
 
   void logout() {
-    String revoke_url = REVOKE_URL + token.data;
+    String revoke_url = REVOKE_URL + auth.token.data;
     var request = new HttpRequest();
     request.open("GET", revoke_url);
     request.onLoad.listen((event) {
       auth.logout();
-      token = null;
       user = null;
     });
     request.send();
   }
 
-  void get_user(auth_token) {
-    token = auth_token;
+  void get_user(_token) {
     endpoint.me.get().then((response) {
       user = response;
     });
