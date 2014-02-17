e391b81323a97fa8682453c3883b7d417cb5000f
ENDOH takanao <djmchl@gmail.com>
2014-02-17 16:06:41 +0700

    refactor

--- gae/dart/lib/timecard_route_initializer.dart
+++ gae/dart/lib/timecard_route_initializer.dart
@@ -4,15 +4,14 @@ class TimecardRouteInitializer implements RouteInitializer {
   init(Router router, ViewFactory view) {
     router.root
       ..addRoute(
-          name: 'logout',
-          path: '/logout',
-          enter: view('views/logout.html')
+          name: "logout",
+          path: "/logout",
+          enter: view("views/logout.html")
       )
-
       ..addRoute(
-          name: 'index',
-          path: '/',
-          enter: view('views/index.html'),
+          name: "index",
+          path: "/",
+          enter: view("views/index.html"),
           defaultRoute: true
       );
   }
