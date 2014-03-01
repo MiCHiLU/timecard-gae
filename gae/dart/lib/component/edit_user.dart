c9fc986012686e735e4a2dea414415b9406cf8e4
ENDOH takanao <djmchl@gmail.com>
2014-03-01 09:54:25 +0700

    fix, c.userが空の時はフォーム編集不可

--- gae/dart/lib/component/edit_user.dart
+++ gae/dart/lib/component/edit_user.dart
@@ -5,10 +5,13 @@ import "package:angular/angular.dart";
 @NgComponent(
   selector: "edit_user-component",
   templateUrl: "packages/timecard_client/component/edit_user.html",
-  cssUrl: "//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css",
+  cssUrl: const ["//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css",
+                 "packages/timecard_client/component/style.css"],
   publishAs: "c"
 )
 class EditUserComponent {
   @NgTwoWay("a")
   var a;
+  @NgTwoWay("user")
+  var user;
 }
